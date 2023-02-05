280537ae731cb07797870cc10c1a683624f86d73
Idaki Milev <idaki@me.com>
2023-02-05 16:08:27 +0100

    Created Array project
    uploaded Lab PB01 Solution

--- Fundamentals/Arrays/src/Exercises/PB01_Day_of_Week.java
+++ Fundamentals/Arrays/src/Exercises/PB01_Day_of_Week.java
@@ -2,7 +2,7 @@ package Exercises;
 
 import java.util.Scanner;
 
-public class PB02 {
+public class PB01_Day_of_Week {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     }
