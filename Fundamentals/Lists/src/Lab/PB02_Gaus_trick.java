2b0865ea987f1d1c98abf45a3e77bc2c78eab385
idaki <idaki@me.com>
2023-02-09 19:49:55 +0100

    update work

---
+++ Fundamentals/Lists/src/Lab/PB02_Gaus_trick.java
@@ -0,0 +1,27 @@
+package Lab;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PB02_Gaus_trick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble).collect(Collectors.toList());
+        List<Double> result = new ArrayList<>();
+//1 2 3 4 5
+        for (int index = 0; index <= numbers.size() / 2 ; index++) {
+
+            numbers.set(index, numbers.get(index) + numbers.get(numbers.size()-1));
+            numbers.remove(numbers.size()-1);
+        }
+        for (Double element:numbers) {
+            System.out.printf("%.0f ", element);
+        }
+        }
+
+}
--- Fundamentals/Lists/src/Lab/PB03.java
+++
@@ -1,9 +0,0 @@
-package Lab;
-
-import java.util.Scanner;
-
-public class PB03 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
