a6fbb31353161d8c0eb9c658c8513080332e1481
idaki <idaki@me.com>
2023-02-10 16:43:07 +0100

    solved problem 5 Array list

--- Fundamentals/Lists/src/Lab/PB02_Gaus_trick.java
+++ Fundamentals/Lists/src/Lab/PB02_Gaus_trick.java
@@ -10,18 +10,20 @@ import java.util.stream.Collectors;
 public class PB02_Gaus_trick {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
-                .map(Double::parseDouble).collect(Collectors.toList());
-        List<Double> result = new ArrayList<>();
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> result = new ArrayList<>();
 //1 2 3 4 5
         for (int index = 0; index <= numbers.size() / 2 ; index++) {
 
             numbers.set(index, numbers.get(index) + numbers.get(numbers.size()-1));
             numbers.remove(numbers.size()-1);
         }
-        for (Double element:numbers) {
+        System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+
+       /* for (Double element:numbers) {
             System.out.printf("%.0f ", element);
-        }
+        }*/
         }
 
 }
--- Fundamentals/Lists/src/Lab/PB09.java
+++
@@ -1,9 +0,0 @@
-package Lab;
-
-import java.util.Scanner;
-
-public class PB09 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
