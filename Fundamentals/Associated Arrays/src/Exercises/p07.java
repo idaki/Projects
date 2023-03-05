a96898fa49fb3a8ff9a3a1da84274618c3c32028
Idaki Milev <idaki@me.com>
2023-03-05 12:50:50 +0100

    maps p4

---
+++ Fundamentals/Associated Arrays/src/Exercises/p07.java
@@ -0,0 +1,9 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class p07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ Fundamentals/Associated Arrays/src/Exercises/p09.java
@@ -0,0 +1,9 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class p09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
