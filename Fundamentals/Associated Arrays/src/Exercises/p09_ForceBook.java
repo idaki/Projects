953f0fef01aef4608b33d4b1c14bbc53bd6991f9
idaki <idaki@me.com>
2023-03-10 21:01:29 +0100

    p09

--- Fundamentals/Associated Arrays/src/Exercises/p09_ForceBook.java
+++ Fundamentals/Associated Arrays/src/Exercises/p09_ForceBook.java
@@ -10,9 +10,10 @@ public class p09_ForceBook {
         Map<String, List> forceMap = new LinkedHashMap<>();
         while (!"Lumpawaroo".equals(input = scanner.nextLine())) {
             boolean isUnique = false;
-
+            boolean isExisting = false;
+            gy
             if (input.contains("|")) {
-                String[] inputArr = input.split("\\|");
+                String[] inputArr = input.split(" \\| ");
                 String forceSide = inputArr[0];
                 String forceUser = inputArr[1];
 
@@ -21,33 +22,51 @@ public class p09_ForceBook {
                         isUnique = true;
                     }
                 }
-                if (!forceMap.containsKey(forceUser) && !isUnique){
-                    forceMap.put(forceSide,new ArrayList<>());
+                if (!forceMap.containsKey(forceUser) && !isUnique) {
+                    forceMap.put(forceSide, new ArrayList<>());
                     forceMap.get(forceSide).add(forceUser);
-                } else if (!isUnique){
+                } else if (!isUnique) {
                     forceMap.get(forceSide).add(forceUser);
                 }
-            } else{
+            } else {
                 String[] inputArr = input.split(" -> ");
-                String forceSide = inputArr[0];
-                String forceUser = inputArr[1];
+                String forceUser = inputArr[0];
+                String forceSide = inputArr[1];
 
                 for (Map.Entry<String, List> entry : forceMap.entrySet()) {
                     if (entry.getValue().contains(forceUser)) {
-                        isUnique = true;
+                        isExisting = true;
                     }
                 }
-                if (!forceMap.containsKey(forceUser) && !isUnique){
-                    forceMap.put(forceSide,new ArrayList<>());
-                    forceMap.get(forceSide).add(forceUser);
-                } else if (!isUnique){
+
+                if (isExisting & forceMap.containsKey(forceSide)) {
+                    for (Map.Entry <String,List> entry : forceMap.entrySet()) {
+                        String force = entry.getKey();
+                        List<String> userList = entry.getValue();
+                        if  (userList.contains(forceUser)){
+                            userList.remove(forceUser);
+                            if (userList.size() == 0){
+                                forceMap.remove(force);
+                            }
+                            forceMap.get(forceSide).add(forceUser);
+                        }
+
+                    }
+                } else if (!isExisting) {
+                   forceMap.get(forceSide).add(forceUser);
+
+                } else if (!forceMap.containsKey(forceSide) & !isExisting){
+                    forceMap.put(forceSide, new ArrayList<>());
                     forceMap.get(forceSide).add(forceUser);
                 }
+                System.out.println(forceUser+" joins the " +forceSide + " side!");
             }
-
-
-            }
-
-
         }
+        forceMap.entrySet().
+                forEach(entry->{
+                    System.out.println("Side: "+entry.getKey()+", Members: "+ entry.getValue().size());
+                    entry.getValue().forEach(forceUser-> System.out.println("! "+ forceUser));
+                });
     }
+}
+
