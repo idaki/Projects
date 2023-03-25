09e4ad179344e3e15dacdaa479886c32af70ea2f
Idaki Milev <idaki@me.com>
2023-03-25 12:32:56 +0100

    p1-2

--- Fundamentals/ExampsPrep/src/E03/p01.java
+++ Fundamentals/ExampsPrep/src/E03/p01.java
@@ -6,32 +6,42 @@ public class p01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String message = scanner.nextLine();
+        StringBuilder updatedMessage = new StringBuilder(message);
 
         String commandInput = "";
 
         while (!"Reveal".equals(commandInput = scanner.nextLine())) {
-            String[] commandArr = commandInput.split("\\:\\|\\:");
+            String[] commandArr = commandInput.split(":\\|:");
             String token = commandArr[1];
             if (commandInput.contains("InsertSpace")) {
                 int index = Integer.parseInt(token);
-
+                if (index >= 0 && index <= updatedMessage.length() - 1) {
+                    updatedMessage.insert(index, " ");
+                }
             } else if (commandInput.contains("Reverse")) {
 
+                if (updatedMessage.toString().contains(token)) {
+                    int startIndex = updatedMessage.indexOf(token);
+                    int endIndex = startIndex + token.length() - 1;
+                    StringBuilder subString = new StringBuilder(updatedMessage.substring(startIndex, endIndex + 1));
+                    subString.reverse();
+                    updatedMessage.delete(startIndex, endIndex+1);
+                    updatedMessage.append(subString);
+                }else{
+                    System.out.println("error");
+                    continue;
+                }
             } else if (commandInput.contains("ChangeAll")) {
-
+                char existingElement =  token.charAt(0);
+                char replacingElement = commandArr[2].charAt(0);
+                String temp = updatedMessage.toString();
+                temp = temp.replace(existingElement,replacingElement);
+                updatedMessage.replace(0,updatedMessage.length(),temp);
             }
-
-            //InsertSpace:|:{index}":
-            //o	Inserts a single space at the given index. The given index will always be valid.
-            //•	"Reverse:|:{substring}":
-            //o	If the message contains the given substring, cut it out, reverse it and add it at the end of the message.
-            //o	If not, print "error".
-            //o	This operation should replace only the first occurrence of the given substring if there are two or more occurrences.
-            //•	"ChangeAll:|:{substring}:|:{replacement}":
-            //o	Changes all occurrences of the given substring with the replacement text.
-
+            System.out.println(updatedMessage);
         }
 
+        System.out.println("You have a new text message: " + updatedMessage);
 
     }
 }
--- Fundamentals/Exams/src/E03_Programming_Fundamentals_Mid_Exam_Retake/PB02.java
+++ Fundamentals/Exams/src/E03_Programming_Fundamentals_Mid_Exam_Retake/PB02.java
@@ -1,7 +1,70 @@
 package E03_Programming_Fundamentals_Mid_Exam_Retake;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PB02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String regex = "[@#]([a-zA-Z]{3,})[@#]{2}\\1[@#]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        int countWords = 0;
+        List <String> printWordsList = new ArrayList<>();
+
+        while (matcher.find()) {
+
+            String currentWordPair = matcher.group("word");
+           currentWordPair = currentWordPair.replace("@","#");
+            int wordLength = currentWordPair.length();
+            int firstIndex = currentWordPair.indexOf("#");
+            int lastIndex = currentWordPair.lastIndexOf("#");
+            int firstWordLength = firstIndex;
+            int secondWorLength = wordLength - 1 - lastIndex;
+
+            if (firstWordLength>=3 && secondWorLength >=3) {
+                countWords++;
+                boolean isDifferent = false;
+                StringBuilder firstWord = new StringBuilder();
+                StringBuilder secondWord = new StringBuilder();
+                for (int i = 0; i < wordLength  / 2 -1; i++) {
+                    isDifferent = false;
+                    char[] wordCharArr = currentWordPair.toCharArray();
+                    char firstWordChar = wordCharArr[i];
+                    char secondWordChar = wordCharArr[wordLength - 1 - i];
+                    firstWord.append(firstWordChar);
+                    secondWord.append(secondWordChar);
+
+                    if ( firstWordChar != secondWordChar)  {
+                        isDifferent = true;
+                        break;
+                    }
+                }
+                if (!isDifferent) {
+                    lastIndex = wordLength / 2;
+                    String currentMatch = String.format(firstWord + " <=> " + secondWord.reverse());
+                    printWordsList.add(currentMatch);
+                }
+            }
+        }
+
+
+        if (!printWordsList.isEmpty()) {
+            System.out.println(countWords+ " word pairs found!");
+            System.out.println("The mirror words are: ");
+            System.out.println(printWordsList.toString().replaceAll("[\\[\\]]", ""));
+        }else if(countWords>0){
+            System.out.println(countWords + " word pairs found!");
+            System.out.println("No mirror words!");
+        }else {
+            System.out.println("No word pairs found!");
+            System.out.println("No mirror words!");
+        }
+
 
     }
 }
