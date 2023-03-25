c6cfe28459bcab3a72971484127f7e9a49094095
Idaki Milev <idaki@me.com>
2023-03-25 13:07:37 +0100

    pr2 solved

--- Fundamentals/Exams/src/E03_Programming_Fundamentals_Mid_Exam_Retake/PB02.java
+++ Fundamentals/Exams/src/E03_Programming_Fundamentals_Mid_Exam_Retake/PB02.java
@@ -10,48 +10,22 @@ public class PB02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String regex = "[@#]([a-zA-Z]{3,})[@#]{2}\\1[@#]";
+        String regex = "(#|@)(?<wordOne>[A-Za-z]{3,})\\1\\1(?<wordTwo>[A-Za-z]{3,})\\1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         int countWords = 0;
         List <String> printWordsList = new ArrayList<>();
 
         while (matcher.find()) {
+            countWords++;
+            StringBuilder firstWord = new StringBuilder(matcher.group("wordOne"));
+            StringBuilder secondWord = new StringBuilder(matcher.group("wordTwo"));
 
-            String currentWordPair = matcher.group("word");
-           currentWordPair = currentWordPair.replace("@","#");
-            int wordLength = currentWordPair.length();
-            int firstIndex = currentWordPair.indexOf("#");
-            int lastIndex = currentWordPair.lastIndexOf("#");
-            int firstWordLength = firstIndex;
-            int secondWorLength = wordLength - 1 - lastIndex;
-
-            if (firstWordLength>=3 && secondWorLength >=3) {
-                countWords++;
-                boolean isDifferent = false;
-                StringBuilder firstWord = new StringBuilder();
-                StringBuilder secondWord = new StringBuilder();
-                for (int i = 0; i < wordLength  / 2 -1; i++) {
-                    isDifferent = false;
-                    char[] wordCharArr = currentWordPair.toCharArray();
-                    char firstWordChar = wordCharArr[i];
-                    char secondWordChar = wordCharArr[wordLength - 1 - i];
-                    firstWord.append(firstWordChar);
-                    secondWord.append(secondWordChar);
-
-                    if ( firstWordChar != secondWordChar)  {
-                        isDifferent = true;
-                        break;
-                    }
-                }
-                if (!isDifferent) {
-                    lastIndex = wordLength / 2;
-                    String currentMatch = String.format(firstWord + " <=> " + secondWord.reverse());
-                    printWordsList.add(currentMatch);
-                }
+            if (firstWord.toString().equals(secondWord.reverse().toString())){
+                String currentMatch = String.format(firstWord + " <=> " + secondWord.reverse());
+                printWordsList.add(currentMatch);
             }
         }
-
 
         if (!printWordsList.isEmpty()) {
             System.out.println(countWords+ " word pairs found!");
