655b03fe4647e7d734bbc64fd35214ad9beac087
Idaki Milev <idaki@me.com>
2024-04-15 18:03:22 +0300

    update parent

---
+++ SpringAppWithModules/src/main/java/bg/softuni/springappwithmodules/SpringAppWithModulesApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.springappwithmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWithModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppWithModulesApplication.class, args);
+    }
+
+}
