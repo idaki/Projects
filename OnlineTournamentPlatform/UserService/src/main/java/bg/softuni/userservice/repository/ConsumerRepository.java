84cbaf1358d1027b753519d74820d6fba3da1bb7
Idaki Milev <idaki@me.com>
2024-04-22 15:40:06 +0300

    backup

---
+++ OnlineTournamentPlatform/UserService/src/main/java/bg/softuni/userservice/repository/ConsumerRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.userservice.repository;
+
+
+import bg.softuni.userservice.models.entity.consumer.Consumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsumerRepository extends JpaRepository<Consumer,Long> {
+}
