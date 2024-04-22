fa69365b0bb2a5a00e576bdfb4eaae5f3f7a6a01
Idaki Milev <idaki@me.com>
2024-04-22 15:39:48 +0300

    clean

--- OnlineTournamentPlatform/UserModule/src/main/java/bg/softuni/usermodule/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.usermodule.repository;
-
-import bg.softuni.usermodule.models.entity.business.employee.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
