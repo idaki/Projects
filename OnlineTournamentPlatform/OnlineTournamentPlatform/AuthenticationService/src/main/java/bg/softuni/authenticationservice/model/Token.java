123e77b468e1679622f171f02610beb48b4bc71d
Idaki Milev <idaki@me.com>
2024-07-13 08:34:22 +0300

    tokens

--- OnlineTournamentPlatform/OnlineTournamentPlatform/AuthenticationService/src/main/java/bg/softuni/authenticationservice/model/Token.java
+++ OnlineTournamentPlatform/OnlineTournamentPlatform/AuthenticationService/src/main/java/bg/softuni/authenticationservice/model/Token.java
@@ -27,4 +27,52 @@ public class Token {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     public User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+
+    public void setRevoked(boolean revoked) {
+        this.revoked = revoked;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- OnlineTournamentPlatform/OnlineTournamentPlatform/AuthenticationService/src/main/java/bg/softuni/authenticationservice/service/impl/JwtServiceImpl.java
+++ OnlineTournamentPlatform/OnlineTournamentPlatform/AuthenticationService/src/main/java/bg/softuni/authenticationservice/service/impl/JwtServiceImpl.java
@@ -1,11 +1,15 @@
 package bg.softuni.authenticationservice.service.impl;
 
+import bg.softuni.authenticationservice.model.Token;
+import bg.softuni.authenticationservice.repositry.TokenRepository;
 import bg.softuni.authenticationservice.service.JwtService;
+import bg.softuni.userservice.repository.UserRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -15,21 +19,43 @@ import java.util.function.Function;
 @Service
 public class JwtServiceImpl extends JwtService {
 
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 //    @Value("${jwt.secret}")
     private String secretKey = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
 
 //    @Value("${jwt.expiration}")
     private long expirationTime = 1000*60*60*1; // in milliseconds
-@Override
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .subject(username)
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS512, secretKey)
-                .compact();
+
+    public JwtServiceImpl(UserRepository userRepository, TokenRepository tokenRepository) {
+        this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
     }
-@Override
+
+    @Override
+    public String generateToken(String username) {
+    String token = Jwts.builder().
+            subject(username)
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + expirationTime))
+            .signWith(getSigninKey())
+            .compact();
+
+        saveTokenToDB(username, token);
+
+        return token;
+    }
+
+    private void saveTokenToDB(String username, String token) {
+        Token newToken = new Token();
+        newToken.setToken(token);
+        newToken.setUser(userRepository.findByUsername(username).get());  // Assume UserService can find a user by username
+        newToken.setExpired(false);
+        newToken.setRevoked(false);
+        tokenRepository.save(newToken);
+    }
+
+    @Override
     public Boolean validateToken(String token, String username) {
         final String usernameFromToken = extractClaim(token, Claims::getSubject);
         return (usernameFromToken.equals(username) && !isTokenExpired(token));
