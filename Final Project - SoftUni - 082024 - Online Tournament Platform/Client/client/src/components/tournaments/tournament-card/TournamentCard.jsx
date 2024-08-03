bda7fdccde28ba81560cf464bebfeb9d73d275cb
Idaki Milev <idaki@me.com>
2024-08-03 09:38:50 +0300

    tournament product page done - view - done

--- Final Project - SoftUni - 082024 - Online Tournament Platform/Client/client/src/components/tournaments/tournament-card/TournamentCard.jsx
+++ Final Project - SoftUni - 082024 - Online Tournament Platform/Client/client/src/components/tournaments/tournament-card/TournamentCard.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './TournamentCard.module.css';
+import ViewContext from '../../../context/viewContext';
 
-export default function TournamentCard({ id, title, description, img, onLearnMore }) {
-    return (
-        <div className="card h-100">
-            <img className="card-img-top" src={img} alt={title} />
-            
-            <div className="card-body p-4">
-                <div className="text-center">
-                    <h5 className="fw-bolder">{title}</h5>
-                    {description}
-                </div>
-            </div>
-            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div className="text-center">
-                    <a className="btn btn-outline-dark mt-auto" href="#" onClick={onLearnMore}>Learn More</a>
-                </div>
-            </div>
+export default function TournamentCard({ id, title, description, img }) {
+  const { setMainContent } = useContext(ViewContext);
+
+  const onLearnMore = (e) => {
+    e.preventDefault();
+    setMainContent('tournament-page');
+  };
+
+  return (
+    <div className="card h-100">
+      <img className="card-img-top" src={img} alt={title} />
+      <div className="card-body p-4">
+        <div className="text-center">
+          <h5 className="fw-bolder">{title}</h5>
+          {description}
         </div>
-    );
+      </div>
+      <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+        <div className="text-center">
+          <a className="btn btn-outline-dark mt-auto" href="#" onClick={onLearnMore}>Learn More</a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- Final Project - SoftUni - 082024 - Online Tournament Platform/Client/client/src/main.jsx
+++ Final Project - SoftUni - 082024 - Online Tournament Platform/Client/client/src/main.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';  // Ensure this import path is correct
 import { AuthProvider } from './context/authContext';  // Ensure this path is correct
 import './config/i18next'
+import { ViewProvider } from './context/viewContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+  <AuthProvider>
+  <ViewProvider>
+    <App />
+  </ViewProvider>
+</AuthProvider>,
  
 );
