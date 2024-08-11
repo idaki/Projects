81f10f6ccd0ca1f16a3a2048d7b6eccdf763d516
Idaki Milev <idaki@me.com>
2024-08-11 10:11:30 +0300

    coverage report

--- Final Project - SoftUni - 082024 - Online Tournament Platform/Server/OnlineTournamentPlatform/CommunicationService/src/main/java/bg/softuni/communicationservice/config/MailConfig.java
+++ Final Project - SoftUni - 082024 - Online Tournament Platform/Server/OnlineTournamentPlatform/CommunicationService/src/main/java/bg/softuni/communicationservice/config/MailConfig.java
@@ -20,8 +20,8 @@ public class MailConfig {
     @Value("${spring.mail.username}")
     private String username;
 
- @Value("${spring.mail.password}")
-    private String password;
+// @Value("${spring.mail.password}")
+    private String password ="nmltimitzjbjeotv";
 
     @Bean
     public JavaMailSender javaMailSender() {
