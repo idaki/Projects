686e302d8533f1a2ccb130d1cde4be35daca22b5
Idaki Milev <idaki.milev@gmail.com>
2025-01-26 22:45:44 +0200

    Init with custom error handle

--- Final Project - SoftUni - 082024 - Online Tournament Platform/Server/OnlineTournamentPlatform/UserService/src/main/java/bg/softuni/userservice/utils/events/Validator/UserExistence/UserExistenceValidatorImpl.java
+++ Final Project - SoftUni - 082024 - Online Tournament Platform/Server/OnlineTournamentPlatform/UserService/src/main/java/bg/softuni/userservice/utils/events/Validator/UserExistence/UserExistenceValidatorImpl.java
@@ -1,5 +1,6 @@
 package bg.softuni.userservice.utils.events.Validator.UserExistence;
 
+import bg.softuni.exceptionhandlerservice.UserAlreadyExistsException;
 import bg.softuni.userservice.repository.UserRepository;
 import bg.softuni.userservice.utils.events.Validator.UserExistence.UserExistenceValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,6 @@ public class UserExistenceValidatorImpl implements UserExistenceValidator {
 
     private final UserRepository userRepository;
 
-    @Autowired
     public UserExistenceValidatorImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -18,14 +18,14 @@ public class UserExistenceValidatorImpl implements UserExistenceValidator {
     @Override
     public void checkIfUsernameExists(String username) {
         if (userRepository.findByUsername(username).isPresent()) {
-            throw new IllegalArgumentException("Username " + username + " already exists.");
+            throw new UserAlreadyExistsException("Username " + username + " already exists.");
         }
     }
 
     @Override
     public void checkIfEmailExists(String email) {
         if (userRepository.findByEmail(email).isPresent()) {
-            throw new IllegalArgumentException("Email " + email + " already exists.");
+            throw new UserAlreadyExistsException("Email " + email + " already exists.");
         }
     }
 }
